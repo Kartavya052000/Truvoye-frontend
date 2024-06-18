373a8fb358183215fa98ddf694b37ee93bd6764e
FTW-Khushal <59960215+FTW-Khushal@users.noreply.github.com>
2024-06-17 17:36:29 -0700

    Created - User Authentication flow frontend
    Created three pages
    + login
    + SignUp
    + Forgot Password
    
    and created routs for it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";   
-import Header from './components/Header';
-import Homepage from './pages/Homepage';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
     <div className="App">
-   <Router> isLoggedIN check locastorage.getItem('token')
+      <Router>
         <Header />
         <Routes>
-        <Route exact path='/' element={<Homepage />} />
-
-
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/forgotpassword" element={<ForgotPassword />} />
         </Routes>
-        </Router>
-        <Routes>
-          Router
-        </Routes>
+      </Router>
     </div>
   );
 }
-// dev notes check a boolean 
+// dev notes check a boolean
 export default App;
