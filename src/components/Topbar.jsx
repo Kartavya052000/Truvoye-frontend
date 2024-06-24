8b67477b7e3f2dc94665de8e08dc9d1231010e49
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-06-24 14:10:55 -0700

    feat setup routes and components for admin-dashbord

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../styles/Topbar.css"
+const Topbar = () => {
+  return (
+    <div className='topbar_container'>
+      Topbar
+    </div>
+  )
+}
+
+export default Topbar
