53eee1b18faf9703f2b23c3d6ee2b590cad6371c
Vatanpreetkaur <Vatanpreet914@gmail.com>
2024-06-26 13:47:19 -0700

    implements requested changes

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -39,7 +39,7 @@ const validationSchema = yup.object({
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = React.useState(false);
-  const [alertMessage, setAlertMessage] = React.useState([]);
+  const [alertMessage, setAlertMessage] = React.useState([null]);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -76,7 +76,7 @@ const SignUp = () => {
               "Verification link sent to your registered email",
             ]);
           }
-          if (response.status === 200) {
+          else if (response.status === 200) {
             setAlertMessage(["error", "Email already registered use Login"]);
           } else {
             setAlertMessage(["error", "Something Went Wrong contact support"]);
@@ -92,7 +92,7 @@ const SignUp = () => {
         });
 
     
-         navigate("/dashboard");
+        // navigate("/dashboard");
     },
   });
 
