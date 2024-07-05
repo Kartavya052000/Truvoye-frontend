bf2e1974482c1775608b6209d9d26e8dba4d3022
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-07-05 00:11:05 -0700

    change:live url for backend apis

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 // src/config.js
 const config = {
-    BASE_SERVER_URL: process.env.REACT_APP_BASE_SERVER_URL || 'http://localhost:4000/api/',
+    // BASE_SERVER_URL: process.env.REACT_APP_BASE_SERVER_URL || 'http://localhost:4000/api/',
+    BASE_SERVER_URL: process.env.REACT_APP_BASE_SERVER_URL,
   };
   
   export default config;
