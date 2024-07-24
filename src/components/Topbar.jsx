a79767ad5c12e9961d207e81155638ff244721b5
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2024-07-24 13:14:24 -0700

    fix:topbar issue fix for driver page

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,252 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import "../styles/Topbar.css";
-// import { Link, useLocation, useNavigate } from "react-router-dom";
-// import { useCookies } from "react-cookie";
-// import logo from "../Assets/imagesV/logo.svg";
-// import profileIcon from "../Assets/imagesV/Profile.svg";
-// //import notificationIcon from "../Assets/imagesV/Notification.svg";
-// import DarkModeToggle from "./DarkModeToggle";
-// import Sidebar from "./Sidebar";
-// import { Cookies } from "react-cookie";
 
-// // -----icons---
-// import analytics from "../Assets/imagesV/Analytics.svg";
-// import orders from "../Assets/imagesV/Orders.svg";
-// import proposals from "../Assets/imagesV/Proposals.svg";
-// import gps from "../Assets/imagesV/GPS.svg";
-// import drivers from "../Assets/imagesV/Truck.svg";
-// import analyticsActive from "../Assets/imagesV/Analytics-active.svg";
-// import ordersActive from "../Assets/imagesV/Orders-active.svg";
-// import proposalsActive from "../Assets/imagesV/Proposals-active.svg";
-// import gpsActive from "../Assets/imagesV/GPS-active.svg";
-// import driversActive from "../Assets/imagesV/Truck-active.svg";
-// import jobSheet from "../Assets/imagesV/jobSheet.svg";
-// import jobSheetActive from "../Assets/imagesV/jobSheetActive.svg";
-// import { Typography, useMediaQuery } from "@mui/material";
-
-// const routeTitles = {
-//   "order-proposal": "Proposal",
-//   analytics: "Analytics",
-//   orders: "Orders",
-//   tracking: "Tracking",
-//   drivers: "Drivers",
-//   "add-driver": "Add Driver",
-//   "edit-driver": "Edit Driver",
-//   "order-details": "Order Details",
-//   "order-tracking": "Order Tracking",
-// };
-// // -----
-// const Topbar = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [isOpenDriver, setIsOpenDriver] = useState(false);
-//   const location = useLocation();
-//   const navigate = useNavigate();
-//   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
-//   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-//   const cookies2 = new Cookies();
-//   const token = cookies2.get("token"); // Adjust based on your auth logic
-//   const [title, setTitle] = useState("");
-//   const isMobile = useMediaQuery("(min-width:600px)");
-
-//   // const location = useLocatio();
-
-//   // const showHamburger = location.pathname.includes('driver');
-
-//   // useEffect(() => {
-//   //   console.log("Cookies: ", cookies);
-//   // }, [cookies]);
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setScreenWidth(window.innerWidth);
-//     };
-
-//     window.addEventListener("resize", handleResize);
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []);
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//     // alert(isOpen)
-//   };
-//   const toggleMenuDriver = () => {
-//     setIsOpenDriver(!isOpenDriver);
-//     // alert(isOpen)
-//   };
-//   const showHamburger = location.pathname.includes("driver");
-//   const dashboard = location.pathname.includes("dashboard");
-
-//   const handleLogout = () => {
-//     removeCookie("token", { path: "/" });
-//     navigate("/");
-//   };
-//   const navItems = [
-//     {
-//       path: "/dashboard/analytics",
-//       name: "Analytics",
-//       defaultIcon: analytics,
-//       activeIcon: analyticsActive,
-//     },
-//     {
-//       path: "/dashboard/orders",
-//       name: "Orders",
-//       defaultIcon: orders,
-//       activeIcon: ordersActive,
-//     },
-//     {
-//       path: "/dashboard/order-proposal",
-//       name: "Proposals",
-//       defaultIcon: proposals,
-//       activeIcon: proposalsActive,
-//     },
-//     {
-//       path: "/dashboard/tracking",
-//       name: "GPS",
-//       defaultIcon: gps,
-//       activeIcon: gpsActive,
-//     },
-//     {
-//       path: "/dashboard/drivers",
-//       name: "Drivers",
-//       defaultIcon: drivers,
-//       activeIcon: driversActive,
-//     },
-//   ];
-//   const navItemsDriver = [
-//     {
-//       path: "/driver/jobsheet",
-//       name: "Job Sheet",
-//       defaultIcon: analytics,
-//       activeIcon: analyticsActive,
-//     },
-//   ];
-
-//   useEffect(() => {
-//     const path = location.pathname;
-//     const segments = path.split("/");
-
-//     // Get the last meaningful segment, ignoring the :id parts
-//     const meaningfulSegment = segments.find((segment) =>
-//       Object.keys(routeTitles).includes(segment)
-//     );
-
-//     // Set the title based on the meaningfulSegment
-//     setTitle(routeTitles[meaningfulSegment] || "Title");
-//   }, [location.pathname]);
-
-//   return (
-//     <>
-//       <div className="topbar_container">
-//         {!showHamburger && <img src={logo} alt="Logo" className="logo-text" />}
-//         {screenWidth < 400 && dashboard && (
-//           <div className="hamburger-menu">
-//             <div
-//               className={`hamburger-icon ${isOpen ? "open" : ""}`}
-//               onClick={toggleMenu}
-//             >
-//               <div className="bar1"></div>
-//               <div className="bar2"></div>
-//               <div className="bar3"></div>
-//             </div>
-//           </div>
-//         )}
-//         {!showHamburger && (
-//           <>
-//             {!isMobile && (
-//               <Typography
-//                 component="h1"
-//                 variant="h6"
-//                 sx={{ width: "100%", textAlign: "center" }}
-//               >
-//                 {title}
-//               </Typography>
-//             )}
-
-//             <div className="icons">
-             
-//               {/* <DarkModeToggle /> */}
-//               <img src={profileIcon} alt="Profile" />
-              
-//               <button
-//                 className="logout_btn"
-//                 onClick={handleLogout}
-//                 variant="contained"
-//                 sx={{ mt: 2, background: "#1237BF" }}
-//               >
-//                 Logout
-//               </button>
-//             </div>
-//           </>
-//         )}
-//         {showHamburger && !dashboard && token && (
-//           <>
-//             <div className="hamburger-menu">
-//               <div
-//                 className={`hamburger-icon ${isOpenDriver ? "open" : ""}`}
-//                 onClick={toggleMenuDriver}
-//               >
-//                 <div className="bar1"></div>
-//                 <div className="bar2"></div>
-//                 <div className="bar3"></div>
-//               </div>
-
-//               {/* <div className={`menu ${isOpenDriver ? "open" : ""}`}>
-//                 <Link to="/driver/pickup">Pickup</Link>
-//                 <Link to="/driver/jobsheet">Job Sheet</Link>
-//               </div> */}
-//             </div>
-
-//             {/* {!token && (
-//               <div className="auth-buttons">
-//                 <Link to="/login" className="btn login-btn">
-//                   Login
-//                 </Link>
-//                 <Link to="/signup" className="btn signup-btn">
-//                   Sign Up
-//                 </Link>
-//               </div>
-//             )} */}
-//           </>
-//         )}
-
-//         {showHamburger && !dashboard && token && (
-//           <>
-//             <div className="hamburger-menu">
-//               <div
-//                 className={`hamburger-icon ${isOpenDriver ? "open" : ""}`}
-//                 onClick={toggleMenuDriver}
-//               >
-//                 <div className="bar1"></div>
-//                 <div className="bar2"></div>
-//                 <div className="bar3"></div>
-//               </div>
-//               {/* <div className={`menu ${isOpenDriver ? 'open' : ''}`}>
-//             <Link to="/driver/pickup">Pickup</Link>
-//             <Link to="/driver/jobsheet">Job Sheet</Link>
-//           </div> */}
-//             </div>
-//             {/* {!token && 
-//          <div className="auth-buttons">
-//          <Link to="/login" className="btn login-btn">Login</Link>
-//          <Link to="/signup" className="btn signup-btn">Sign Up</Link>
-//        </div>
-// } */}
-//           </>
-//         )}
-//       </div>
-//       {isOpen ? (
-//         <Sidebar show={true} navItems={navItems}  />
-//       ) : (
-//         ""
-//       )}
-//       {isOpenDriver ? <Sidebar show={true} navItems={navItemsDriver} /> : ""}
-//       {/* <div >
-//    </div> */}
-//     </>
-//   );
-// };
-
-// export default Topbar;
 import React, { useEffect, useState } from "react";
 import "../styles/Topbar.css";
 import { Link, useLocation, useNavigate } from "react-router-dom";
@@ -324,7 +76,7 @@ const Topbar = () => {
 
   const showHamburger = location.pathname.includes("driver");
   const dashboard = location.pathname.includes("dashboard");
-
+const driverpage =location.pathname.includes("dashboard/drivers")
   const handleLogout = () => {
     removeCookie("token", { path: "/" });
     navigate("/");
@@ -396,7 +148,7 @@ const Topbar = () => {
   return (
     <>
       <div className="topbar_container">
-        {!showHamburger && <img src={logo} alt="Logo" className="logo-text" />}
+        {(!showHamburger || driverpage) && <img src={logo} alt="Logo" className="logo-text" />}
         {screenWidth < 400 && dashboard && (
           <div className="hamburger-menu">
             <div
@@ -409,7 +161,8 @@ const Topbar = () => {
             </div>
           </div>
         )}
-        {!showHamburger && (
+        
+        {(!showHamburger || driverpage)   && (
           <>
             {!isMobile && (
               <Typography
